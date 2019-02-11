951707c3e6436d37694e72e11cf617683e43ff43
Dafna <39526021+Dafnochka@users.noreply.github.com>
2019-02-11 23:25:52 +0300

    Веб версия

---
+++ precache-manifest.75e65a6c42f378b98547df98e7dce85c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "151841658955c573febc",
+    "url": "/static/js/main.15184165.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "151841658955c573febc",
+    "url": "/static/css/main.14d57adc.chunk.css"
+  },
+  {
+    "revision": "b8c20efc44746294d74a2e6594d16720",
+    "url": "/index.html"
+  }
+];
